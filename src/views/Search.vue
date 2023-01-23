c3fda3d6e2649ad0d08f8cf4b9b5045255456242
Shyngys Sailau <shino_099@mail.ru>
2023-01-23 21:44:19 +0600

    Добавил картинки

---
+++ src/views/Search.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        <img src="../assets/Search/shape.png" alt="">
+        <input type="text" name="" id="" placeholder="Поиск">
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+/*import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}*/
+</script>
